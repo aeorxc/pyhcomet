e0f81901d5df009cf80ecffc1a7eb2a958db1e11
ui906879 <gibran.afzal@rwe.com>
2022-10-14 16:17:02 +0100

    fixing test case

--- tests/test_slates.py
+++ tests/test_slates.py
@@ -1,11 +1,12 @@
-from pyhcomet import slates
-from pyhcomet import create_slate
 import pandas as pd
-import pytest
+import time
+from pyhcomet import slates
+
 
 def test_get_slates():
     res = slates.get_slates()
     assert res is not None
+
 
 def test_get_slate():
     res = slates.get_slate(25458)
@@ -13,14 +14,16 @@ def test_get_slate():
 
 
 def test_post_slate():
-    slate_name = pd.to_datetime('now').strftime('%y%m%d%I%M%S')
-    assays = ["Agbami '07", "Agbami (GSC) July '18", "Agbami (CVX) Mar 16 '21"]
+    slate_name = f"test_slate_{pd.to_datetime('now', utc=True).strftime('%y%m%d%I%M%S')}"
+    assays = ["Agbami '07", "Agbami (GSC) July '18"]
     template = {
         "SlateItems": assays,
         "Name": slate_name
     }
     res = slates.post_slate(template)
     assert res == 'Created'
+    time.sleep(1)
     allslates = slates.get_slates()
     testslate = allslates[allslates['Name'] == slate_name]
-    slates.delete_slate(testslate['ID'].iloc[0])
+    res = slates.delete_slate(testslate['ID'].iloc[0])
+    assert res == 'No Content'
