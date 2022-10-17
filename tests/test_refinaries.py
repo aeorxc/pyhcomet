6db68cd9c5660440e997b44e77fb1b8315a39633
ui906879 <gibran.afzal@rwe.com>
2022-10-17 13:25:29 +0100

    get post_case() working

--- tests/test_refinaries.py
+++ tests/test_refinaries.py
@@ -1,19 +1,21 @@
 from pyhcomet import refineries
-import pytest
+
 
 def test_get_refinary_list():
-    res = refineries.get_refinary_list('NWE','GERMANY')
+    res = refineries.get_refinary_list('NWE', 'GERMANY')
     assert res is not None
 
+
 def test_get_refinary():
-    res = refineries.get_refinary('NWE',72687)
+    res = refineries.get_refinary('NWE', 72687)
     assert res is not None
+
 
 def test_get_refinary_configs():
     res = refineries.get_refinary_configs('NWE')
     assert res is not None
 
-def test_get_refinary_config():
-    res = refineries.get_refinary_config('NWE',72193)
-    assert res is not None
 
+def test_get_refinary_config():
+    res = refineries.get_refinary_config('NWE', 72193)
+    assert res is not None
