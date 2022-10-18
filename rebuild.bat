50f4dd99ea314221a875fc76930bd9ba25542dea
ui906879 <gibran.afzal@rwe.com>
2022-10-18 14:44:36 +0100

    add .github actions

--- rebuild.bat
+++ rebuild.bat
@@ -1,1 +1,3 @@
-pip uninstall -y pyhcometpython setup.py bdist_wheelpip install C:\dev\pyplattsapi\dist\pyhcomet-0.0.3-py3-none-any.whl
+pip uninstall -y pyhcomet
+python setup.py bdist_wheel
+pip install C:\dev\pyhcomet\dist\pyhcomet-0.0.3-py3-none-any.whl
