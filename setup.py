9972f7a8a1d58aeef0af37eb648543c2d97db5c7
ui906879 <gibran.afzal@rwe.com>
2023-03-14 10:37:48 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyhcomet",
-    version="0.0.8",
+    version="0.0.9",
     author="aeorxc",
     description="Wrapper around Haverly HComet",
     url="https://github.com/aeorxc/pyhcomet",
